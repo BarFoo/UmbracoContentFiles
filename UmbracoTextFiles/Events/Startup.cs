75fd863d3c7c9c1f339635cb96f9078278a2cebd
Dave Parr <davesparr@gmail.com>
2017-06-15 21:09:02 +0100

    Accommodate for when the Umbraco db is unavailable!

--- UmbracoTextFiles/Events/Startup.cs
+++ UmbracoTextFiles/Events/Startup.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections.Generic;
+using System.Data.SqlClient;
 using Umbraco.Core;
+using Umbraco.Core.Logging;
 using Umbraco.Core.Models;
 using Umbraco.Core.Services;
 using Umbraco.Web.Routing;
@@ -26,25 +28,34 @@ namespace UmbracoTextFiles.Events
             ContentService.UnPublished += textFileContentEvents.UnPublished;
             
             var contentTypeService = applicationContext.Services.ContentTypeService;
-            var textFileContentType = contentTypeService.GetContentType("textFile");
-            if (textFileContentType == null)
+
+            try
             {
-                var textboxMultipleDef = new DataTypeDefinition(-1, "Umbraco.TextboxMultiple");
-
-                var contentType = new ContentType(-1)
+                var textFileContentType = contentTypeService.GetContentType("textFile");
+                if (textFileContentType == null)
                 {
-                    Alias = "textFile",
-                    Name = "Text File",
-                    Icon = "icon-files"
-                };
+                    var textboxMultipleDef = new DataTypeDefinition(-1, "Umbraco.TextboxMultiple");
 
-                var propertyType = new PropertyType(textboxMultipleDef, "textFileContent");
-                propertyType.Name = "Content";
+                    var contentType = new ContentType(-1)
+                    {
+                        Alias = "textFile",
+                        Name = "Text File",
+                        Icon = "icon-files"
+                    };
 
-                contentType.AddPropertyGroup("File Content");
-                contentType.AddPropertyType(propertyType, "File Content");
+                    var propertyType = new PropertyType(textboxMultipleDef, "textFileContent");
+                    propertyType.Name = "Content";
 
-                contentTypeService.Save(contentType);
+                    contentType.AddPropertyGroup("File Content");
+                    contentType.AddPropertyType(propertyType, "File Content");
+
+                    contentTypeService.Save(contentType);
+                }
+            }
+            catch (SqlException sqlEx)
+            {
+                // Umbraco is most likely not installed yet!
+                LogHelper.Error<Startup>("Unable to create doc type for text file because Umbraco db is unavailable!", sqlEx);
             }
         }
     }
