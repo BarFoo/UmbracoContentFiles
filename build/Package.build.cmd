7a3ae854c80c94336c66f68ab0e892066f5f8a1f
Dave Parr <davesparr@gmail.com>
2017-06-17 16:15:11 +0100

    Fixing build issues

--- build/Package.build.cmd
+++ build/Package.build.cmd
@@ -1,2 +1,2 @@
-nuget restore ../src/UmbracoContentFiles.sln
+build\Tools\nuget.exe restore ../src/UmbracoContentFiles.sln
 C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\msbuild.exe Package.build.xml
